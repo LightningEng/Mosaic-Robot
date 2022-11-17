f6409a607b2f07cfeaa3b999210c1f1236f880b2
massderrico <massderrico@yahoo.com>
2022-11-16 23:35:05 -0500

    Final algo done

--- project/algorithm_mechanism.py
+++ project/algorithm_mechanism.py
@@ -11,7 +11,16 @@ from time import sleep
 from math import pi
 
 DELAY_SEC = 1.5  # seconds of delay between measurements
+                #j
+input_array = [ [0,0,1,0,1], #i
+                [0,1,1,1,0],
+                [0,1,1,0,1],
+                [0,1,1,1,0],
+                [0,0,0,0,1]]
 
+#x values starting left to right
+x_values = [0,-115, -230, -345, -460]
+y_values = [-710, -600,-480,-360,-240]
 
 print("Program start.\nWaiting for sensors to turn on...")
 
@@ -37,80 +46,27 @@ def algorithm():
         
         LEFT_WHEEL.set_limits(MOTOR_POWER)
         RIGHT_WHEEL.set_limits(MOTOR_POWER)
-            
-        
-                
+               
         TANK_WHEEL.set_limits(MOTOR_POWER)
         
-        
-        
-        #Move
-        
-        LEFT_WHEEL.set_position(0)
-        RIGHT_WHEEL.set_position(0)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(110)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(-710)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(600)
-        sleep(DELAY_SEC)
-        
-        
-        LEFT_WHEEL.set_position(-115)
-        RIGHT_WHEEL.set_position(-115)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(110)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(-600)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(490)
-        sleep(DELAY_SEC)
-        
-        
-        LEFT_WHEEL.set_position(-230)
-        RIGHT_WHEEL.set_position(-230)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(110)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(-480)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(370)
-        sleep(DELAY_SEC)
-        
-        
-        LEFT_WHEEL.set_position(-345)
-        RIGHT_WHEEL.set_position(-345)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(110)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(-360)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(250)
-        sleep(DELAY_SEC)
-        
-        
-        LEFT_WHEEL.set_position(-460)
-        RIGHT_WHEEL.set_position(-460)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(110)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(-240)
-        sleep(DELAY_SEC)
-        TANK_WHEEL.set_position_relative(130)
-        sleep(DELAY_SEC)
-        
-        
-        LEFT_WHEEL.set_position(0)
-        RIGHT_WHEEL.set_position(0)
-        sleep(DELAY_SEC)
-#         
-        
-        
-        
-        
-        
-
+        for i in range(0,len(input_array)):
+            if sum(input_array[i]) > 0:  #if there are any 1 in the currrent row
+                LEFT_WHEEL.set_position(x_values[i]) #move arm to that row
+                RIGHT_WHEEL.set_position(x_values[i])
+                sleep(DELAY_SEC)
+                TANK_WHEEL.set_position_relative(110) #retract claw to drop cube
+                sleep(DELAY_SEC)
+                tmp = input_array[i].copy() 
+                get_last = 4 - tmp.reverse().index(1) #find positon last 1 in current row
+            for j in range(len(input_array[i])): #iterate through the columns
+                if input_array[i][j] == 1:
+                    TANK_WHEEL.set_position_relative(y_values[j]) #place cube is proper column
+                    sleep(DELAY_SEC)
+                    if j == get_last:
+                        TANK_WHEEL.set_position_relative(-1*(y_values[j])-110) #if current position is in final 1 in row , retract arm back but block next cube from dropping
+                        sleep(DELAY_SEC)
+                    else:
+                        TANK_WHEEL.set_position_relative(-1*y_values[j]+10) #retract arm all the way and next cube drops into claw
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
         pass
     finally:
